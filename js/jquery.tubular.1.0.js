36b10c39078ac3d063106728668676a775ebcf47
seanschang <seanchang.stat@gmail.com>
2015-02-11 12:45:05 -0500

    default unmute

--- js/jquery.tubular.1.0.js
+++ js/jquery.tubular.1.0.js
@@ -18,7 +18,7 @@
     var defaults = {
         ratio: 16/9, // usually either 4/3 or 16/9 -- tweak as needed
         videoId: 'ZCAnLxRvNNc', // toy robot in space is a good default, no?
-        mute: true,
+        mute: false,
         repeat: true,
         width: $(window).width(),
         wrapperZIndex: 99,
