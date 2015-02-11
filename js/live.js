e84b1a6cda4e98ad8f3410a842d3ef5d1a1e0da2
seanschang <seanchang.stat@gmail.com>
2015-02-11 18:32:39 -0500

    live link

---
+++ js/live.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'oFkSye506-w', start: 1 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
