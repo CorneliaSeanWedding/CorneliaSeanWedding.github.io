bb8ce876e7c312434003a218c181012ff4299169
seanschang <seanchang.stat@gmail.com>
2015-04-23 11:00:57 -0400

    links

--- js/live.js
+++ js/live.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'oFkSye506-w', start: 1 };
+	var options = { videoId: 'BB_TcXu30Y4', start: 1 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
